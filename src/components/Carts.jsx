983e395cd3833a2dc9c05cc45f3a0888454ba7d6
mursalinmirme <mursalinmir02@gmail.com>
2023-09-15 09:29:21 +0600

    I have make static cart accounts design for showing selected cart list, remaining hour, total credit and total price accouts

--- src/components/Carts.jsx
+++ src/components/Carts.jsx
@@ -2,7 +2,17 @@ import './Carts.css';
 const Carts = () => {
     return (
       <div className="cart-wrapper">
-        
+        <h3 className='cart-remaining'>Credit Hour Remaining 14 hr</h3>
+        <div className='course-list-container'>
+        <h2 className='cart-list-title'>Course Name</h2>
+        <ul className='cart-list'>
+            <li>1 : Introduction to c programming</li>
+            <li>1 : Introduction to c programming</li>
+            <li>1 : Introduction to c programming</li>
+        </ul>
+        </div>
+        <h4 className='total-credit-hour'>Total Credit Hour : 05</h4>
+        <h4 className='total-price'>Total Price : 15000 Taka</h4>
       </div>
     );
 };
